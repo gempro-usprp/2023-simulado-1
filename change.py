ca621a343c3c5c48f2413938b95670b9b22fbcd9
Gabriel Bergoc <gabriel.bergoc@outlook.com>
2023-03-18 17:55:35 -0300

    change: correções

--- change.py
+++ change.py
@@ -1,14 +1,17 @@
 T = int(input())
 
 for t in range(T):
-    D, N = map(lambda x : int(x), input().split(' '))
-    prices = map(lambda x : float(x), input().split(' '))
+    D, N = map(lambda x: int(x), input().split(' '))
+    prices = map(lambda x: float(x), input().split(' '))
 
     maxChange = 0
     for price in prices:
-        nProducts = D // price;
-        change = (D / price - nProducts) * price;
-        maxChange = max(maxChange, change);
+        nProducts = D // price
+
+        if (nProducts == 0):
+            continue
+
+        change = (D / price - nProducts) * price
+        maxChange = max(maxChange, change)
 
     print(f"{maxChange:.2f}")
-    
