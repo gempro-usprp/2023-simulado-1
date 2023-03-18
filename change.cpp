21e6976aa3264145f74e219ab146e242f4a0dd33
Gabriel Bergoc <gabriel.bergoc@outlook.com>
2023-03-18 18:07:55 -0300

    change.cpp: correção

--- change.cpp
+++ change.cpp
@@ -1,25 +1,35 @@
 #include <iostream>
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
-int main() {
- 
+
+int main()
+{
+
     int T;
     cin >> T;
-    
-    for (int t = 0; t < T; t++) {
+    cout << setprecision(2) << fixed;
+
+    for (int t = 0; t < T; t++)
+    {
         int D, N;
         cin >> D >> N;
 
-        float x;
+        float price;
         float maxChange = 0;
-        for (int n = 0; n < N; n++) {
-            cin >> x;
+        for (int n = 0; n < N; n++)
+        {
+            cin >> price;
 
-            float nProducts = D / (int) x;
-            float change = (D / x - nProducts) * x;
-            maxChange = max<float>(maxChange, change);
+            int nProducts = D / price;
+
+            if (nProducts == 0)
+            {
+                continue;
+            }
+
+            float change = (D / price - nProducts) * price;
+            maxChange = max(maxChange, change);
         }
 
         cout << maxChange << endl;
